e09bfceb585d6bb1c014e677dfe26d37d89450a2
ModeClearCode <62543799+ModeClearCode@users.noreply.github.com>
2022-02-02 17:16:02 +0300

    Update openvas_exporter.py
    - added logging
    - added TLS Connection support (not tested)
    - added exception handling for better survivability
    - fixed some bugs

--- openvas_exporter.py
+++ openvas_exporter.py
@@ -1,20 +1,29 @@
 import datetime
 import os
 import argparse
+import logging
 from datetime import date, timedelta
 import time
 from statistics import mean
 from lxml.etree import Element
 from gvm.connections import SSHConnection
+from gvm.connections import TLSConnection
+from gvm.errors import GvmError
 from gvm.protocols.gmp import Gmp
 from gvm.transforms import EtreeTransform
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('openvas_exporter')
+
 
 class OpenvasCollector:
 
-    def __init__(self, args):
+    def __init__(self, args, i):
+
+        self.i = i
+        self.args = args
         # Setup time range
         self.time_interval = args.time_interval
         now = datetime.datetime.now()
@@ -23,17 +32,47 @@ class OpenvasCollector:
         self.server_ip = args.host
 
         # Create connection
-        self.conn = SSHConnection(
-            hostname=args.host,
-            username=args.user,
-            password=args.ssh_pass,
-            port=args.ssh_port)
-        transform = EtreeTransform()
-        with Gmp(self.conn, transform=transform) as self.gmp:
-            self.gmp.authenticate(args.login, args.openvas_pass)
+        if args.connection.lower() == 'ssh':
+            if args.user is None:
+                logger.error('User for ssh on OpenVAS server is not set.')
+                exit(1)
+            self.conn = SSHConnection(
+                hostname=args.host,
+                username=args.user,
+                password=args.ssh_pass,
+                port=args.ssh_port,
+                timeout=30
+            )
+        elif args.connection.lower() == 'tls':
+            if args.cert_file is None or args.ca_file is None or args.private_key is None:
+                logger.error('Parameters for tls-connection on OpenVAS server is not set.')
+                exit(1)
+            self.conn = TLSConnection(
+                hostname=args.host,
+                certfile=args.cert_file,
+                cafile=args.ca_file,
+                keyfile=args.private_key,
+                password=args.pass_key,
+                port=args.tls_port,
+                timeout=30
+            )
+        else:
+            logger.error('Unknown connection type. Use ssh or tls.')
+            exit(1)
+        try:
+            transform = EtreeTransform()
+            with Gmp(self.conn, transform=transform) as self.gmp:
+                self.gmp.authenticate(args.login, args.openvas_pass)
+        except GvmError as e:
+            logger.error(f'Connection is failed. Details: {e}')
+            exit(1)
 
     def collect(self):
-
+        now = datetime.datetime.now()
+        if self.to_date == date(now.year, now.month, now.day):
+            logger.debug(f'Refreshing date...')
+            self.to_date = date(now.year, now.month, now.day) + timedelta(days=1)
+            self.from_date = self.to_date - timedelta(days=self.time_interval)
         metrics_total = {
             'low':
                 GaugeMetricFamily('openvas_total_low', 'Total sum low-level vulnerabilities per last'
@@ -74,6 +113,7 @@ class OpenvasCollector:
         sum_medium = 0
         sum_high = 0
         index_list = []
+
         results = self.get_results_xml()
         result_hosts = get_result_hosts(results)
 
@@ -93,22 +133,22 @@ class OpenvasCollector:
                                                               values_list[4])
         for metric in metrics_host.values():
             yield metric
-
-        max_index = max(index_list)
-        avg = max_index - mean(index_list)
-        sec_index = (max_index ** 2 - avg) ** 0.5
-        result_sums = {'low': sum_low, 'medium': sum_medium, 'high': sum_high,
-                       'sec_index': sec_index}
-        print(
-            f'Summary of results from {self.from_date.isoformat()} '
-            f'to {self.to_date.isoformat()}'
-        )
-        print(f'High: {int(sum_high)}')
-        print(f'Medium: {int(sum_medium)}')
-        print(f'Low: {int(sum_low)}')
-        for key in result_sums:
-            metrics_total[key].add_metric([self.server_ip], result_sums[key])
-            yield metrics_total[key]
+        if len(result_hosts) > 0:
+            max_index = max(index_list)
+            avg = max_index - mean(index_list)
+            sec_index = (max_index ** 2 - avg) ** 0.5
+            result_sums = {'low': sum_low, 'medium': sum_medium, 'high': sum_high,
+                           'sec_index': sec_index}
+            logger.info(
+                f'Summary of results from {self.from_date.isoformat()} '
+                f'to {(self.to_date - timedelta(days=1)).isoformat()}'
+            )
+            logger.info(f'High: {int(sum_high)}')
+            logger.info(f'Medium: {int(sum_medium)}')
+            logger.info(f'Low: {int(sum_low)}')
+            for key in result_sums:
+                metrics_total[key].add_metric([self.server_ip], result_sums[key])
+                yield metrics_total[key]
 
     def get_results_xml(self) -> Element:
         # Getting the Results in the defined time period
@@ -116,7 +156,19 @@ class OpenvasCollector:
             f'levels=hml rows=-1 created>{self.from_date.isoformat()} and '
             f'created<{self.to_date.isoformat()}'
         )
-        return self.gmp.get_results(filter_string=report_filter)
+        try:
+            results = self.gmp.get_results(filter_string=report_filter)
+        except (GvmError, BrokenPipeError, OSError) as e:
+            if self.i == 4:
+                logger.error('Cannot connect after 3 retries. Quitting...')
+                exit(1)
+            logger.warning(f'Caught exception! Connection # {self.i} was corrupted. Details: {e}\n'
+                           f'Trying to reconnect...')
+            self.__init__(self.args, self.i + 1)
+            self.get_results_xml()
+        else:
+            self.i = 1
+            return results
 
 
 def get_result_hosts(results_xml: Element):
@@ -142,7 +194,7 @@ def get_result_hosts(results_xml: Element):
                 table_data[ip] = [hostname, 0, 1, 0, (severity ** 4) / 1000, [name]]
             elif 7 <= severity <= 10:
                 table_data[ip] = [hostname, 1, 0, 0, (severity ** 4) / 1000, [name]]
-    print(f'Founded {len(table_data)} host results.\n')
+    logger.info(f'Founded {len(table_data)} host results.\n')
     return table_data
 
 
@@ -164,32 +216,68 @@ def list_add(res_list, severity, name):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-o', '--host',
+        '-o', '--hostname',
         dest='host',
-        required=True,
-        help='Openvas host ip',
-        default=os.environ.get('OPENVAS_SSH_HOST')
+        required=False,
+        help='DNS name or IP address of the remote OpenVAS server',
+        default=os.environ.get('OPENVAS_HOSTNAME')
     )
     parser.add_argument(
         '-u', '--user',
         dest='user',
-        required=True,
-        help='User for ssh on openvas host',
+        required=False,
+        help='User for ssh on OpenVAS server. Required for ssh-type connection.',
         default=os.environ.get('OPENVAS_SSH_USER')
     )
     parser.add_argument(
         '-ss', '--ssh-secret',
         dest='ssh_pass',
-        required=True,
-        help='Password for ssh on openvas host',
+        required=False,
+        help='Password for ssh on OpenVAS server. Required for ssh-type connection.',
         default=os.environ.get('OPENVAS_SSH_PASSWORD')
+    )
+    parser.add_argument(
+        '-cf', '--cert-file',
+        dest='cert_file',
+        required=False,
+        help='Path to PEM encoded certificate file. Required for tls-type connection.',
+        default=os.environ.get('OPENVAS_TLS_CERT')
+    )
+    parser.add_argument(
+        '-ca', '--ca-file',
+        dest='ca_file',
+        required=False,
+        help='Path to PEM encoded CA file. Required for tls-type connection.',
+        default=os.environ.get('OPENVAS_TLS_CA')
+    )
+    parser.add_argument(
+        '-pk', '--private-key',
+        dest='private_key',
+        required=False,
+        help='Path to PEM encoded private key. Required for tls-type connection.',
+        default=os.environ.get('OPENVAS_TLS_KEY')
+    )
+    parser.add_argument(
+        '-ppk', '--pass-key',
+        dest='pass_key',
+        required=False,
+        help='Password for the private key. Required for tls-type connection, if set.',
+        default=os.environ.get('OPENVAS_TLS_PASS_KEY')
+    )
+    parser.add_argument(
+        '-tp', '--tls-port',
+        dest='tls_port',
+        required=False,
+        type=int,
+        help='Port for tls on OpenVAS server. Required for tls-type connection. Default = 9390',
+        default=os.environ.get('OPENVAS_TLS_PORT', '9390')
     )
     parser.add_argument(
         '-sp', '--ssh-port',
         dest='ssh_port',
         required=False,
         type=int,
-        help='Port for ssh on openvas host. Default = 22',
+        help='Port for ssh on OpenVAS server. Required for ssh-type connection. Default = 22',
         default=os.environ.get('OPENVAS_SSH_PORT', '22')
     )
     parser.add_argument(
@@ -203,15 +291,15 @@ def parse_args():
     parser.add_argument(
         '-l', '--login',
         dest='login',
-        required=True,
-        help='login for openvas api',
+        required=False,
+        help='Login for GSA',
         default=os.environ.get('OPENVAS_USER')
     )
     parser.add_argument(
         '-os', '--openvas-secret',
         dest='openvas_pass',
-        required=True,
-        help='password for openvas api',
+        required=False,
+        help='Password for GSA',
         default=os.environ.get('OPENVAS_PASSWORD')
     )
     parser.add_argument(
@@ -230,15 +318,35 @@ def parse_args():
         help='Scan time interval, in days. Default = 14 days',
         default=os.environ.get('OPENVAS_TIME_INTERVAL', '14')
     )
+    parser.add_argument(
+        '-c', '--connection',
+        dest='connection',
+        required=False,
+        help='OpenVAS connection type to use by exporter. Options: ssh or tls. Default = ssh',
+        default=os.environ.get('OPENVAS_CONN_TYPE', 'ssh')
+    )
+    parser.add_argument(
+        '-d', '--debug',
+        dest='debug',
+        action='store_true',
+        required=False,
+        help='Enable debug output. Default = False',
+        default=os.environ.get('OPENVAS_EXPORTER_DEBUG', False)
+    )
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+    if args.host is None:
+        logger.error('Hostname/IP of OpenVAS server is not set.')
+        exit(1)
     try:
-        print("Connecting to Openvas on: " + args.host)
+        logger.info("Connecting to Openvas on: " + args.host)
         start_http_server(args.port)
-        REGISTRY.register(OpenvasCollector(args))
+        REGISTRY.register(OpenvasCollector(args, 1))
         while True:
             time.sleep(args.scr_interval)
     except KeyboardInterrupt:
